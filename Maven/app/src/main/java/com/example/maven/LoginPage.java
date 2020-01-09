28a0541feea727fe3dfce87676c25205a961b680
jjomena <joaquinmena.84@gmail.com>
2020-01-09 15:48:39 -0600

    Inicializando Proyecto

---
+++ Maven/app/src/main/java/com/example/maven/LoginPage.java
@@ -0,0 +1,29 @@
+package com.example.maven;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LoginPage.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link LoginPage#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginPage extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_page, container, false);
+    }
+}
